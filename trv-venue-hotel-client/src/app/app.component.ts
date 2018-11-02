f29b85d7c72303581b26fc8419e6adc706660e99
Mariam <mariam.a.qasim@gmail.com>
2018-11-02 11:57:56 -0400

    rendering data

--- trv-venue-hotel-client/src/app/app.component.ts
+++ trv-venue-hotel-client/src/app/app.component.ts
@@ -5,25 +5,25 @@ import { Http } from '@angular/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'TRV Venue Hotel Search';
-
-  bookhotelList: Bookhotel[];
+  hotel = [];
   apiURL = 'http://localhost/3000/hotels'
 
-  constructor(private http: Http) {
+  bookhotelList: Bookhotel[];
 
-    console.log('please work');
-
-  }
+  constructor(private http: Http) { }
 
   getBookhotelData() {
     this.http.get('http://localhost:3000/hotels')
       .subscribe(res => this.bookhotelList =
         res.json() as Bookhotel[]);
 
+    console.log(this);
+
   }
 }
+
 
